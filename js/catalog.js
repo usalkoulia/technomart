3a92f29bb156a4f237a00be4bd49d224af8bdd14
Ulia Usalko <usalko.ulia@gmail.com>
2017-11-28 16:16:57 +0200

    Исправление мелких багов

---
+++ js/catalog.js
@@ -0,0 +1,11 @@
+var button_buy = document.querySelector(".button-buy");
+var basket_popup = document.querySelector(".basket-popup");
+var feedback_button_basket_popup = document.querySelector(".feedback-button-basket-popup");
+
+button_buy.addEventListener("click", function() {
+  basket_popup.classList.add("basket-popup-show");
+});
+
+feedback_button_basket_popup.addEventListener("click", function() {
+  basket_popup.classList.remove("basket-popup-show");
+});
