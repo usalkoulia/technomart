53d1044bec5fe732f43feb809718d185ddc90dc0
Ulia Usalko <usalko.ulia@gmail.com>
2017-11-28 12:42:29 +0200

    Оживила второй слайдер

--- js/main.js
+++ js/main.js
@@ -1,17 +1,68 @@
+var slide_button_circle_left = document.querySelector(".button-circle-left");
+var slide_button_circle_right = document.querySelector(".button-circle-right");
+var features_slide_1 = document.querySelector(".features-slide-1");
+var features_slide_2 = document.querySelector(".features-slide-2");
 var slide_button_arrow_left = document.querySelector(".button-arrow-left");
-var slide_button_arrow_right = document.querySelector(".button-arrow-right");
-var slide_1 = document.querySelector(".features-slide-1");
-var slide_2 = document.querySelector(".features-slide-2");
+var slide_button_arrow_right= document.querySelector(".button-arrow-right");
+var services_item_1 = document.querySelector("#services-item-1");
+var services_item_2 = document.querySelector("#services-item-2");
+var services_item_3 = document.querySelector("#services-item-3");
+var services_slide_1 = document.querySelector(".services-slide-1");
+var services_slide_2 = document.querySelector(".services-slide-2");
+var services_slide_3 = document.querySelector(".services-slide-3");
+var contacts_link = document.querySelector(".contacts-link");
 
+slide_button_circle_left.addEventListener("click", function() {
+  slide_button_circle_left.classList.add("circle-active");
+  slide_button_circle_right.classList.remove("circle-active");
+  features_slide_1.classList.add("active-slide");
+  features_slide_2.classList.remove("active-slide");
+});
+
+slide_button_circle_right.addEventListener("click", function() {
+  slide_button_circle_right.classList.add("circle-active");
+  slide_button_circle_left.classList.remove("circle-active");
+  features_slide_2.classList.add("active-slide");
+  features_slide_1.classList.remove("active-slide");
+});
 
 slide_button_arrow_left.addEventListener("click", function() {
-  slide_button_arrow_left.classList.add("arrow-active");
-  slide_1.classList.add(".active-slide");
-  slide_2.classList.remove(".active-slide");
+  slide_button_circle_left.classList.add("circle-active");
+  slide_button_circle_right.classList.remove("circle-active");
+  features_slide_1.classList.add("active-slide");
+  features_slide_2.classList.remove("active-slide");
 });
 
 slide_button_arrow_right.addEventListener("click", function() {
-  slide_button_arrow_right.classList.add("arrow-active");
-  slide_2.classList.add(".active-slide");
-  slide_1.classList.remove(".active-slide");
+  slide_button_circle_right.classList.add("circle-active");
+  slide_button_circle_left.classList.remove("circle-active");
+  features_slide_2.classList.add("active-slide");
+  features_slide_1.classList.remove("active-slide");
+});
+
+services_item_1.addEventListener("click", function() {
+  services_item_1.classList.add("item-active");
+  services_item_2.classList.remove("item-active");
+  services_item_3.classList.remove("item-active");
+  services_slide_1.classList.add("active-slide");
+  services_slide_2.classList.remove("active-slide");
+  services_slide_3.classList.remove("active-slide");
+});
+
+services_item_2.addEventListener("click", function() {
+  services_item_2.classList.add("item-active");
+  services_item_1.classList.remove("item-active");
+  services_item_3.classList.remove("item-active");
+  services_slide_2.classList.add("active-slide");
+  services_slide_1.classList.remove("active-slide");
+  services_slide_3.classList.remove("active-slide");
+});
+
+services_item_3.addEventListener("click", function() {
+  services_item_3.classList.add("item-active");
+  services_item_1.classList.remove("item-active");
+  services_item_2.classList.remove("item-active");
+  services_slide_3.classList.add("active-slide");
+  services_slide_1.classList.remove("active-slide");
+  services_slide_2.classList.remove("active-slide");
 });
